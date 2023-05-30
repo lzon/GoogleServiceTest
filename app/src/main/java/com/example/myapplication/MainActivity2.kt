814aceaf0ae47343b5082d50543e52c6261a19e4
like <stevensliliana94@gmail.com>
2023-05-30 09:50:16 +0400

    init commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity2 : AppCompatActivity() {
+    var content: ByteArray? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        Utils.cacheContext = this
+        setContentView(R.layout.activity_main2)
+        content = ByteArray(1000 * 1000 * 40)
+    }
+}
